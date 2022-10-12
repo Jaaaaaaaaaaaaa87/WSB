6f8805e7ce755fa1c30fcf84526f689370f50db8
a <ErdeMO@interia.pl>
2022-10-12 08:25:44 +0200

    select 1

---
+++ db/scripts/1_select.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Użytkownicy z tabeli users</h3>
+    <?php
+     ?>
+  </body>
+</html>
